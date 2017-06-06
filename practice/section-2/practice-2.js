841e3990dfe2b2a77ec8c191777d4d1de1af4057
Lover Almost <Lover Almost>
2017-06-06 21:29:22 +0800

    [xurao]add section-2 practice 2

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,55 @@
 'use strict';
-
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var collection2 = [];
+  for(var i = 0; i< collection.length; i ++)
+  {
+    if(collection[i].length!==1)
+    {
+      var arr = collection[i].split('-');
+      for(var j = 0;j<arr[1];j++)
+      {
+        collection2.push(arr[0]);
+      }
+    }
+    else {
+      collection2.push(collection[i]);
+    }
+  }
+  return test1(collection2);
+}
+
+function check(element,result) {
+  for(var i = 0;i<result.length;i++)
+  {
+    if (element == result[i].key)
+    {
+      return 1;
+    }
+  }
+  return 0;
+}
+function find(element,result) {
+  for(var i = 0;i<result.length;i++)
+  {
+    if (element == result[i].key)
+    {
+      return i;
+    }
+  }
+
+}
+function test1(collection) {
+  var result = [];
+  for(var i = 0; i< collection.length; i ++)
+  {
+    if(!check(collection[i],result))
+    {
+      result.push({key:collection[i],count:1});
+    }
+    else {
+      result[find(collection[i],result)].count++;
+    }
+  }
+  return result;
+
 }
