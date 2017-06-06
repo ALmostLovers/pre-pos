65ad0db4c5a2566665ab24e84a1f45fe07f8ccd7
Lover Almost <Lover Almost>
2017-06-06 17:37:24 +0800

    sectionA

---
+++ practice/section-1/practice-4.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function collectSameElements(collectionA, objectB) {
+
+    var c = new Array();
+    var collectionB=objectB.value;
+    for(var i = 0; i<collectionA.length ; i++)
+    {
+      for(var j = 0; j<collectionB.length ; j++)
+      {
+        if (collectionA[i].key == collectionB[j])
+        {
+          c.push(collectionB[j]);
+          break;
+        }
+      }
+    }
+    return c;
+
+}
---
+++ practice/section-2/practice-1.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function countSameElements(collection) {
+  var element = collection[0];
+  for(var i = 0; i< collection; i ++)
+  {
+    if (collection)
+    if(collection[i]==collection[i+1])
+    {
+      count++
+    }
+    key = collection
+  }
+  return '实现练习要求，并改写该行代码。';
+}
